f820b211cc8f03db95306d26a916a33b43f55dd3
毛春江 <mcj@weiyian.com>
2020-03-27 16:42:27 +0800

    demo功能添加

--- flutter_example/lib/Untils/CacheUntil/cache.dart
+++ flutter_example/lib/Untils/CacheUntil/cache.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cache/cache_until.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,9 @@ class CachePage extends StatefulWidget {
 class _CachePageState extends State<CachePage> {
   String allSpace;
   String remainingSpace;
+  String _systemCache;
+  bool _isClean;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -21,7 +26,7 @@ class _CachePageState extends State<CachePage> {
     // TODO: implement reassemble
     super.reassemble();
     getAllSpace();
-
+    getSystemCache();
   }
 
   void getAllSpace() async {
@@ -34,24 +39,43 @@ class _CachePageState extends State<CachePage> {
     });
   }
 
+  void getSystemCache() async {
+    String cc;
+    if (Platform.isIOS) {
+//      cc = await Cache.systemCache("");
+    } else if (Platform.isAndroid) {
+      cc = await Cache.systemCache();
+    }
+    setState(() {
+      _systemCache = cc;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cache'),
         actions: <Widget>[
-          PopupMenuButton<String>(
-            onSelected: (String text){
-              if(text == '1'){
-                Navigator.of(context).pushNamed('doc', arguments: 'https://github.com/wya-team/flutter_wya/blob/master/flutter_untils/cache/lib/cache_until.dart');
-              } else if (text == '2'){
-                Navigator.of(context).pushNamed('doc', arguments: 'https://www.baidu.com');
-              }
-            },
-              itemBuilder: (BuildContext context) {
+          PopupMenuButton<String>(onSelected: (String text) {
+            if (text == '1') {
+              Navigator.of(context).pushNamed('doc',
+                  arguments:
+                      'https://github.com/wya-team/flutter_wya/blob/master/flutter_untils/cache/lib/cache_until.dart');
+            } else if (text == '2') {
+              Navigator.of(context)
+                  .pushNamed('doc', arguments: 'https://www.baidu.com');
+            }
+          }, itemBuilder: (BuildContext context) {
             return [
-              PopupMenuItem(child: Text('查看文档'), value: '1',),
-              PopupMenuItem(child: Text('查看Demo'), value: '2',),
+              PopupMenuItem(
+                child: Text('查看文档'),
+                value: '1',
+              ),
+              PopupMenuItem(
+                child: Text('查看Demo'),
+                value: '2',
+              ),
             ];
           }),
         ],
@@ -73,6 +97,23 @@ class _CachePageState extends State<CachePage> {
                 children: <Widget>[
                   Text('总空间:$allSpace'),
                   Text('可用空间:$remainingSpace'),
+                  Text('缓存大小:$_systemCache'),
+                  RaisedButton(
+                    child: Text("清理缓存"),
+                    onPressed: () async {
+                      bool isClean;
+                      if (Platform.isIOS) {
+//                        isClean = await Cache.clearCache("");
+                      } else if (Platform.isAndroid) {
+                        isClean = await Cache.clearCache();
+                      }
+                      setState(() {
+                        if (isClean) {
+                          _systemCache = '0B';
+                        }
+                      });
+                    },
+                  ),
                 ],
               ),
             ],
