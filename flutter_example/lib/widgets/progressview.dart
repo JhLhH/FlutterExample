e8c213cde7785e65600e66b0194b86f6974938e5
毛春江 <mcj@weiyian.com>
2020-04-28 13:50:41 +0800

    progressview demo

--- flutter_example/lib/widgets/progressview.dart
+++ flutter_example/lib/widgets/progressview.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterexample/customwidget/popupmenbutton_page.dart';
+import 'package:progressview/linearprogressview.dart';
 import 'package:progressview/progressview.dart';
 
 class ProgressViewWidgetPage extends StatefulWidget {
@@ -35,20 +36,56 @@ class _ProgressViewWidgetPageState extends State<ProgressViewWidgetPage>
         ],
       ),
       body: Center(
-        child: ProgressViewWidget(
-          colors: [
-            Colors.red,
-            Colors.amber,
-            Colors.cyan,
-            Colors.green[200],
-            Colors.blue,
-            Colors.red
+        child: Column(
+          children: <Widget>[
+            ProgressViewWidget(
+              colors: [
+                Colors.red,
+                Colors.amber,
+                Colors.cyan,
+                Colors.green[200],
+                Colors.blue,
+                Colors.red
+              ],
+              radius: 100.0,
+              strokeWidth: 8.0,
+              strokeCapRound: true,
+              value: 0.9,
+              totalAngle: 1 * pi,
+            ),
+            LinearProgressViewWidget(
+              height: 5,
+              width: 100,
+              backgroundColor: Colors.grey,
+              value: 0.3,
+              valueColor: Colors.green,
+              type: -1,
+            ),
+            LinearProgressViewWidget(
+              height: 5,
+              width: 250,
+              backgroundColor: Colors.grey,
+              value: 0.3,
+              valueColor: Colors.blue,
+              type: 0,
+            ),
+            LinearProgressViewWidget(
+              height: 5,
+              width: 250,
+              backgroundColor: Colors.grey,
+              value: 0.5,
+              valueColor: Colors.red,
+              type: 1,
+            ),
+            LinearProgressViewWidget(
+              height: 5,
+              width: 250,
+              backgroundColor: Colors.grey,
+              value: 0.3,
+              valueColor: Colors.blue,
+              type: 2,
+            ),
           ],
-          radius: 100.0,
-          strokeWidth: 8.0,
-          strokeCapRound: true,
-          value: 0.9,
-          totalAngle: 1 * pi,
         ),
       ),
     );
